179d7bbcea930defae62f3c47b938f17bc1703e2
vinicioscastro <viniciospcastro19@gmail.com>
2022-04-12 13:50:59 -0300

    typeAnnotation

---
+++ mod02/typeAnnotation.js
@@ -0,0 +1,10 @@
+"use strict";
+let nome = "vinicios";
+let idade = 22;
+let curso = "ciência da computação";
+let animais = ["elefante", "gato", "cachorro"];
+console.log(animais);
+// => objetos
+let carro;
+carro = { nome: "prisma", ano: 2015, preco: 55000 };
+console.log(carro.ano);
---
+++ mod02/typeAnnotation.ts
@@ -0,0 +1,17 @@
+let nome: string = "vinicios";
+let idade: number = 22;
+let curso: string = "ciência da computação";
+
+let animais: string[] = ["elefante", "gato", "cachorro"];
+console.log(animais);
+
+// => objetos
+
+let carro: {
+  nome: string;
+  ano: number;
+  preco: number;
+};
+
+carro = { nome: "prisma", ano: 2015, preco: 55000 };
+console.log(carro.ano);
